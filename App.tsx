bc7f7b1d9347c6f5ef516aed07726e88ba595022
Renan Pereira Loureiro <renan.pereiral@sonda.com>
2023-02-09 23:17:18 -0300

    build: gesture-handler and reanimated

--- App.tsx
+++ App.tsx
@@ -2,13 +2,17 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { Text, View } from 'react-native'
 
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-purple-700">
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView className="flex-1">
+      <View className="flex-1 justify-center items-center">
+        <Text className="text-purple-700">
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </GestureHandlerRootView>
   )
 }

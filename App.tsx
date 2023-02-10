2e61ae773000022e906ddcf6dfc39e8c64a3e95c
Renan Pereira Loureiro <renan.pereiral@sonda.com>
2023-02-09 23:02:31 -0300

    build: eslint and prettier

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-purple-700">
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}

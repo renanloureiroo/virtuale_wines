12a8a54ef12284bc4c06924b26d83e45d09a09b4
Renan Pereira Loureiro <renanloureiro.dev@gmail.com>
2023-02-23 11:54:34 -0300

    build: stup flipper
    install and config flipper in android and ios

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import {
+  NavigationContainer,
+  useNavigationContainerRef
+} from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { useFlipper } from '@react-navigation/devtools'
 
 export default function App() {
+  const navigationRef = useNavigationContainerRef()
+
+  useFlipper(navigationRef)
+
   return (
     <GestureHandlerRootView className="flex-1">
-      <NavigationContainer>
+      <NavigationContainer ref={navigationRef}>
         <View className="flex-1 justify-center items-center">
           <Text className="text-purple-700">
             Open up App.js to start working on your app!

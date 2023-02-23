ff2f36fb2d4bd62dfa9389133f9caafb2628a3c1
Renan Pereira Loureiro <renanloureiro.dev@gmail.com>
2023-02-23 13:06:09 -0300

    feat(singin): signin screen

---
+++ app/features/Authentication/Screens/SignIn.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const SignIn = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-purple-600"> SignIn Screen</Text>
+    </View>
+  )
+}

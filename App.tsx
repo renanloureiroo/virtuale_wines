e528c8820d0b1901465c14c42ea58ae79d610270
Renan Pereira Loureiro <renanloureiro.dev@gmail.com>
2023-02-23 09:43:21 -0300

    build: add react-navigation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
@@ -7,12 +8,14 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler'
 export default function App() {
   return (
     <GestureHandlerRootView className="flex-1">
-      <View className="flex-1 justify-center items-center">
-        <Text className="text-purple-700">
-          Open up App.js to start working on your app!
-        </Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <View className="flex-1 justify-center items-center">
+          <Text className="text-purple-700">
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+      </NavigationContainer>
     </GestureHandlerRootView>
   )
 }

f9fc46554b861990e909e9e09983444913456b72
Renan Pereira Loureiro <renanloureiro.dev@gmail.com>
2023-02-23 13:07:02 -0300

    refactor(app.tsx): moved navigation container for appstack

--- App.tsx
+++ App.tsx
@@ -1,29 +1,14 @@
 import React from 'react'
-import {
-  NavigationContainer,
-  useNavigationContainerRef
-} from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
 
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { useFlipper } from '@react-navigation/devtools'
+import { AppStackNavigation } from './app/navigation/AppStackNavigation'
 
 export default function App() {
-  const navigationRef = useNavigationContainerRef()
-
-  useFlipper(navigationRef)
-
   return (
     <GestureHandlerRootView className="flex-1">
-      <NavigationContainer ref={navigationRef}>
-        <View className="flex-1 justify-center items-center">
-          <Text className="text-purple-700">
-            Open up App.js to start working on your app!
-          </Text>
-          <StatusBar style="auto" />
-        </View>
-      </NavigationContainer>
+      <StatusBar style="auto" />
+      <AppStackNavigation />
     </GestureHandlerRootView>
   )
 }

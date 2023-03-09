a1fc46003914ae8d37f8e1aa4f1186048978a900
Renan Pereira Loureiro <renanloureiro.dev@gmail.com>
2023-03-08 21:04:25 -0300

    chore: add Icon component

--- app/components/Icon/index.tsx
+++ app/components/Icon/index.tsx
@@ -1,10 +1,19 @@
 import React, { FC } from 'react'
 import { IconProps as PIconsProps, User } from 'phosphor-react-native'
+import { styled } from 'nativewind'
 
 type IconProps = PIconsProps & {
   Icon: typeof User
+  className?: string
 }
 
-export const Icon: FC<IconProps> = ({ Icon, ...props }) => {
-  return <Icon {...props} />
+export const Icon: FC<IconProps> = ({ Icon, className, ...props }) => {
+  const StyledIcon = styled(Icon)
+
+  return (
+    <StyledIcon
+      className={className}
+      {...props}
+    />
+  )
 }
